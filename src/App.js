38eda1afb5a98b846b59bbaf79aa7b3553643316
sujin3473 <sujin3473@gmail.com>
2022-03-08 12:13:32 +0900

    navbar 스타일, Link 설정

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import './App.css';
-import NavBar from 'components/navBar';
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+
+import NavBar from "components/navBar";
+import AddProduct from "containers/AddProduct";
+import ProductList from "containers/ProductList";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Routes>
+        <Route path="/" element={<ProductList />} />
+        <Route path="/add/product" element={<AddProduct />} />
+      </Routes>
     </div>
   );
 }
--- src/components/navBar.jsx
+++ src/components/navBar.jsx
@@ -1,19 +1,20 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <header className="navBar">
-      <img src="img/amondz_logo.png" alt="아몬즈로고" className='navBar-logo'/>
+      <img src="img/amondz_logo.png" alt="아몬즈로고" className="navBar-logo" />
       <ul>
         <li>
-          상품 리스트
+          <Link to="/add/product">상품 등록</Link>
         </li>
         <li>
-          상품 등록
+          <Link to="/">상품 리스트</Link>
         </li>
       </ul>
-    </header>  
+    </header>
   );
-}
+};
 
 export default NavBar;
