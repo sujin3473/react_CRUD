28ba93ae2b680e6ceb6f7fe452a1402d84de7878
sujin3473 <sujin3473@gmail.com>
2022-03-08 14:46:44 +0900

    상품 등록 포맷 추가

--- src/containers/AddProduct/index.jsx
+++ src/containers/AddProduct/index.jsx
@@ -1,7 +1,57 @@
-import React from "react";
+import React, { useState } from "react";
 
 const AddProduct = () => {
-  return <>add product</>;
+  const [name, setName] = useState("");
+  const [price, setPrice] = useState(0);
+  const [desc, setDesc] = useState("");
+
+  const onChangeName = (e) => {
+    setName(e.target.value);
+  };
+
+  const onChangePrice = (e) => {
+    setPrice(e.target.value);
+  };
+
+  const onChangeDesc = (e) => {
+    setDesc(e.target.value);
+  };
+
+  const handleAdd = (e) => {
+    const data = { price, desc };
+    console.log(data);
+    localStorage.setItem(name, JSON.stringify(data));
+  };
+
+  const onImgChange = () => {};
+
+  return (
+    <>
+      <h2>상품 등록</h2>
+      <div className="form-control">
+        <label>상품명</label>
+        <input type="text" name="name" onChange={onChangeName} />
+      </div>
+      <div className="form-control">
+        <label>가격</label>
+        <input type="text" name="price" onChange={onChangePrice} />
+      </div>
+      <div className="form-control">
+        <label>상품 설명</label>
+        <input type="text" name="description" onChange={onChangeDesc} />
+      </div>
+      <div className="form-control">
+        <label>상품 이미지</label>
+        <input
+          type="file"
+          name="image"
+          accept="image/*"
+          onChange={onImgChange}
+        />
+      </div>
+      <button onClick={handleAdd}>추가</button>
+    </>
+  );
 };
 
 export default AddProduct;
